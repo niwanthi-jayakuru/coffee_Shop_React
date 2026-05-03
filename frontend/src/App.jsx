404c19c3ea82def63b051164e6616cb87ab4b42e
Niwanthi Jayakuru <niwanthijayakuru@gmail.com>
2026-05-03 22:52:03 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Navbar from './components/Navbar/Navbar';
+const App = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+      });
+    });
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return<div>
+    <div className="container bg-red-500">
+        <div className="flex">
+            {/* Logo section */}
+            <div>logo</div>
+
+            {/* links section */}
+
+        </div>
+    </div>
+  </div>
+  
+}
+
+export default Navbar
